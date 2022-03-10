6b000e12b0e8f5f78ab2c7e093f75db0df8bfb95
trader-daniil <74257219+trader-daniil@users.noreply.github.com>
2022-03-10 11:19:44 +0300

    corrected function

--- parsing_jobs.py
+++ parsing_jobs.py
@@ -5,6 +5,7 @@ import requests
 from dotenv import load_dotenv
 from terminaltables import AsciiTable
 
+
 LANGUAGES = [
     'JavaScript',
     'Java',
@@ -40,37 +41,42 @@ def get_hh_vacancies_by_language(language, page=1):
     return response.json()
 
 
-def count_avg_salary(salary_from, salary_to):
-    avg_salary = (salary_from + salary_to) / 2
-    return avg_salary
-
-
-def predict_rub_salary_for_hh(vacancy):
-    if not vacancy['salary'] or vacancy['salary']['currency'] != 'RUR':
+def predict_rub_salary(currency, salary_from, salary_to):
+    if not salary_from and not salary_to:
         return None
-    salary = vacancy['salary']
-    if salary['from'] and salary['to']:
-        return count_avg_salary(
-            salary_from=salary['from'],
-            salary_to=salary['to'],
-        )
-    elif salary['from'] and not salary['to']:
-        return salary['from'] * 1.2
-    return salary['to'] * 0.8
+    elif salary_from and salary_to:
+        return (salary_from + salary_to) / 2
+    elif salary_from and not salary_to:
+        return salary_from * 1.2
+    return salary_to * 0.8
 
 
 def parse_language_statistics_hh(language):
     language_stat = {language: {}}
     language_vacancies = get_hh_vacancies_by_language(language=language)
     language_stat[language]['vacancies_found'] = language_vacancies['found']
+    total_pages = language_vacancies['pages']
     salaries = []
-    for vacancy_page in range(1, 21):
+    for vacancy_page in range(total_pages):
         vacancies_in_page = get_hh_vacancies_by_language(
             language=language,
             page=vacancy_page,
-        )['items']
-        for vacancy in vacancies_in_page:
-            avg_salary = predict_rub_salary_for_hh(vacancy=vacancy)
+        )
+        if vacancies_in_page['found'] == 0:
+            language_stat[language]['vacancies_processed'] = 0
+            language_stat[language]['average_salary'] = 0
+            return language_stat
+        for vacancy in vacancies_in_page['items']:
+            if not vacancy['salary'] or vacancy['salary']['currency'] != 'RUR':
+                continue
+            salary_currency = vacancy['salary'].get('currency', None)
+            salary_from = vacancy['salary'].get('from', None)
+            salary_to = vacancy['salary'].get('to', None)
+            avg_salary = predict_rub_salary(
+                currency=salary_currency,
+                salary_from=salary_from,
+                salary_to=salary_to,
+            )
             if not avg_salary:
                 continue
             salaries.append(avg_salary)
@@ -100,35 +106,36 @@ def get_superjob_vacancies_by_language(language, superjob_token, page=0):
     return response.json()
 
 
-def predict_rub_salary_for_superjob(vacancy):
-    if vacancy['currency'] != 'rub' or (
-            vacancy['payment_from'] == 0 and vacancy['payment_to'] == 0):
-        return None
-    elif vacancy['payment_from'] and vacancy['payment_to']:
-        return count_avg_salary(
-            salary_from=vacancy['payment_from'],
-            salary_to=vacancy['payment_to'],
-        )
-    elif vacancy['payment_from'] and not vacancy['payment_to']:
-        return vacancy['payment_from'] * 1.2
-    return vacancy['payment_to'] * 0.8
-
-
 def parse_language_staistics_superjob(language, superjob_token):
     language_stat = {}
     language_stat[language] = {}
     salaries = []
-    for page in range(0, 500):
+    more = True
+    page = 0
+    while more:
         vacancies = get_superjob_vacancies_by_language(
             language=language,
             superjob_token=superjob_token,
             page=page,
         )
-        if vacancies['total'] == 0:
-            break
+        more = vacancies['more']
+        page += 1
         language_stat[language]['vacancies_found'] = vacancies['total']
+        if not vacancies['total']:
+            language_stat[language]['vacancies_processed'] = 0
+            language_stat[language]['average_salary'] = 0
+            return language_stat
         for vacancy in vacancies['objects']:
-            avg_salary = predict_rub_salary_for_superjob(vacancy=vacancy)
+            salary_currency = vacancy.get('currency', None)
+            if not salary_currency or salary_currency != 'rub':
+                continue
+            salary_from = vacancy.get('payment_from', None)
+            salary_to = vacancy.get('payment_to', None)
+            avg_salary = predict_rub_salary(
+                currency=salary_currency,
+                salary_from=salary_from,
+                salary_to=salary_to,
+            )
             if not avg_salary:
                 continue
             salaries.append(avg_salary)
@@ -151,9 +158,9 @@ def create_table_with_languages(languages_stat, service_name):
     for language, language_stat in languages_stat.items():
         language_stat = [
             language,
-            str(language_stat['vacancies_found']),
-            str(language_stat['vacancies_processed']),
-            str(language_stat['average_salary']),
+            language_stat['vacancies_found'],
+            language_stat['vacancies_processed'],
+            language_stat['average_salary'],
         ]
         languages_info.append(language_stat)
     vacancies_table = AsciiTable(languages_info)
